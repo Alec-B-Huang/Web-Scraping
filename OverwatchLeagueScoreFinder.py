8351ef8ea2b88d4b45f86479faf9b389eee7e3b1
Alec-B-Huang <67602734+Alec-B-Huang@users.noreply.github.com>
2022-12-26 10:28:03 -0800

    Still fixing

--- OverwatchLeagueScoreFinder.py
+++ OverwatchLeagueScoreFinder.py
@@ -5,20 +5,24 @@ from selenium.webdriver.common.by import By
 # URL
 url = ("https://overwatchleague.com/en-us/schedule?stage=regular_season&")
 
+# Ask which week to be viewed out of a total of 27 weeks
+week = int(input('Which week do you want to view, there are 27 weeks? '))
+
 # Obtaining data from url
-driver = webdriver.Chrome(' ') # Insert webdriver path in ()
+driver = webdriver.Chrome(' ')# Insert file path
 driver.get(url)
 
 # Giving time to find div class
 driver.implicitly_wait(15)
 
 # Finding match results
-match = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[3]/div[3]/div[1]/div[2]/div[1]/div/section/div[3]/div[3]/div/div[2]')
+xpath, xpath2 = '//*[@id="__next"]/div/div/div[3]/div[3]/div[1]/div[2]/div[', ']/div/section/div[3]'
+fullXpath = xpath + str(week) + xpath2
+match = driver.find_element(By.XPATH, fullXpath)
 
 # Formatting output
 scores = match.text
 results = ' '.join(scores.split('\n'))
 
 print(results)
-
 
